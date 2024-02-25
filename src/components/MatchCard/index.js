627f709a77ebcedb9657610ef92f7b3d38eafd1b
TanmayGhosh347 <tanmay2690@gmail.com>
2024-02-25 16:36:19 +0000

    third commmit

---
+++ src/components/MatchCard/index.js
@@ -0,0 +1,26 @@
+import './index.css'
+
+const MatchCard = props => {
+  const {matchDetails} = props
+  const {competingTeamLogo, competingTeam, matchStatus, result} = matchDetails
+  const getMatchStatusClassName = status =>
+    status === 'Won' ? 'match-won' : 'match-lost'
+  const matchStatusClassName = `match-status ${getMatchStatusClassName(
+    matchStatus,
+  )}`
+
+  return (
+    <li className="match-item">
+      <img
+        src={competingTeamLogo}
+        className="competing-teams-logo"
+        alt={`competing team ${competingTeam}`}
+      />
+      <p className="competing-teams-name">{competingTeam}</p>
+      <p className="result">{result}</p>
+      <p className={matchStatusClassName}>{matchStatus}</p>
+    </li>
+  )
+}
+
+export default MatchCard
